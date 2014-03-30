8e4c1e925d0257560fb49ba8bb0a8ff7bd77bb9d
Raymond T. Hightower <rth@wisdomgroup.com>
2014-03-30 10:09:29 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EsSandbox::Application.config.secret_key_base = '2fa5a22e965d14cf5d1d3c1311dfd71b4de054c8804ec8277a54f6d2fe5d6324be27fbec7ab29c022b85dace938c09dcdb12e5dcaebc4ffbc23252916b37f55c'
